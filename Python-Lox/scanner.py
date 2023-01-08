2d3ca32fe69c5a21e69fd4f798a2b51b824460e8
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2023-01-07 23:10:15 -0500

    Adding some token handling to scanner

--- Python-Lox/scanner.py
+++ Python-Lox/scanner.py
@@ -75,44 +75,111 @@ class Scanner():
         Appends an EOF (end of file) token to self.tokens
         Returns self.tokens list
         """
-        for char in self.source:
+        while not self._is_at_eof():
             self._start = self._current
             self._scanToken()
 
-        self.tokens.append(Token(TokenType.EOF, "", None, self._line))
+        self.tokens.append(Token(TokenType.EOF, "", None, len(self._line)))
         return self.tokens
 
-    def _scanToken(self):
+    def _scanToken(self) -> None:
         """
         Arguments are stored in Scanner class instance
         Switch statement to add each token to self.tokens
         """
         char = self._advance()
         match char:
+            # Handling single character lexemes
             case '(':
-                self.tokens.append(Token(TokenType.LEFT_PAREN, '(', '(', self._line))
+                self._addToken(TokenType.LEFT_PAREN)
             case ')':
-                self.tokens.append(Token(TokenType.RIGHT_PAREN, ')', ')', self._line))
+                self._addToken(TokenType.RIGHT_PAREN)
             case '{':
-                self.tokens.append(Token(TokenType.LEFT_BRACE, '{', '{', self._line))
+                self._addToken(TokenType.LEFT_BRACE)
             case '}':
-                self.tokens.append(Token(TokenType.RIGHT_BRACE, '}', '}', self._line))
+                self._addToken(TokenType.RIGHT_BRACE)
             case ',':
-                self.tokens.append(Token(TokenType.COMMA, ',', ',', self._line))
+                self._addToken(TokenType.COMMA)
             case '.':
-                self.tokens.append(Token(TokenType.DOT, '.', '.', self._line))
+                self._addToken(TokenType.DOT)
             case '-':
-                self.tokens.append(Token(TokenType.MINUS, '-', '-', self._line))
+                self._addToken(TokenType.MINUS)
             case '+':
-                self.tokens.append(Token(TokenType.PLUS, '+', '+', self._line))
+                self._addToken(TokenType.PLUS)
             case ';':
-                self.tokens.append(Token(TokenType.SEMICOLON, ';', ';', self._line))
+                self._addToken(TokenType.SEMICOLON)
             case '*':
-                self.tokens.append(Token(TokenType.STAR, '*', '*', self._line))
+                self._addToken(TokenType.STAR)
 
+            # Handling potentially double character lexemes
+            case '!':
+                self._addToken(TokenType.BANG_EQUAL if self._match('=') else TokenType.BANG)
+            case '=':
+                self._addToken(TokenType.EQUAL_EQUAL if self._match('=') else TokenType.EQUAL)
+            case '<':
+                self._addToken(TokenType.LESS_EQUAL if self._match('=') else TokenType.LESS)
+            case '>':
+                self._addToken(TokenType.GREATER_EQUAL if self._match('=') else TokenType.GREATER)
+
+            # Logic for ignoring parsing of comments
+            case '/':
+                if self._match('/'):
+                    while self._peek() != '\n' and not self._is_at_eof():
+                        self._advance()
+                else:
+                    self._addToken(TokenType.SLASH)
+            
+            # Handling whitespace characters
+            case ' ':
+                None
+            case '\r':
+                None
+            case '\t':
+                None
+            case '\n':
+                self._line += 1
+
+            # Processing literals
+            case '"':
+                self._addToken(TokenType.STRING)
+
+            # Error in default case
             case _:
                 Lox.error(self._line, "Unexpected character")
             
 
-    def _advance(self):
-        return self.source[self._current+1]
+    def _advance(self) -> str:
+        self._current += 1
+        return self.source[self._current-1]
+
+    def _peek(self) -> str:
+        """
+        Checks the current character without advancing
+        """
+        if self._is_at_eof():
+            return '\0'
+        return self.source[self._current]
+
+    def _addToken(self, tokenType: TokenType, literal:str = None) -> None:
+        text = self.source[self._start:self._current]
+        self.tokens.append(Token(tokenType, text, literal, self._line))
+
+    def _match(self, expected):
+        """
+        Checks if the current character matches an expected character
+        Returns False if at end of file or if character does not match
+        Consumes current character and returns True otherwise
+        """
+        if self._is_at_eof():
+            return False        
+        elif self.source[self._current] != expected:
+            return False
+        
+        self._current += 1
+        return True
+        
+    def _is_at_eof(self):
+        """
+        Checks whether whole source file has been parsed
+        """
+        return (self._current >= len(self.source))
