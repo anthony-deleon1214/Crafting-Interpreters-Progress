85e594e6ef43b461cf2977c5e48f850e46276c8f
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2023-01-13 15:06:18 -0500

    Adding parser methods

--- Python-Lox/lox_parser.py
+++ Python-Lox/lox_parser.py
@@ -59,6 +59,7 @@ class Parser:
     def _expression(self):
         return self._equality()
 
+    # Binary operator methods
     def _equality(self):
         """
         Matches based on the grammar rule
@@ -73,3 +74,87 @@ class Parser:
             expr = grammar.Binary(expr, operator, right)
 
         return expr
+
+    def _comparison(self):
+        expr = self._term()
+
+        # Matching for any comparison operator
+        while self._match(scanner.TokenType.GREATER, scanner.TokenType.GREATER_EQUAL, scanner.TokenType.LESS, scanner.TokenType.LESS_EQUAL):
+            operator = self._previous()
+            right = self._term()
+            expr = grammar.Binary(expr, operator, right)
+
+        return expr
+
+    def _term(self):
+        expr = self._factor()
+
+        # Matching for addition and subtraction tokens
+        while self._match(scanner.TokenType.PLUS, scanner.TokenType.MINUS):
+            operator = self._previous()
+            right = self._factor()
+            expr = grammar.Binary(expr, operator, right)
+
+        return expr
+
+    def _factor(self):
+        expr = self._unary()
+
+        # Matching for multiplication and division
+        while self._match(scanner.TokenType.SLASH, scanner.TokenType.STAR):
+            operator = self._previous()
+            right = self._unary()
+            expr = grammar.Binary(expr, operator, right)
+
+        return expr
+
+    # Unary operator methods
+    def _unary(self):
+        """
+        Check for unary operators (negations here)
+        Return Unary syntax tree if found
+        Call self._primary otherwise
+        """
+        if self._match(scanner.TokenType.BANG, scanner.TokenType.MINUS):
+            operator = self._previous()
+            right = self._unary()
+            return grammar.Unary(operator, right)
+        
+        return self._primary()
+
+    def _primary(self):
+        """
+        Checking for terminal expressions
+        Returns Literal syntax tree node with corresponding Python type
+        """
+        # Check for boolean and null types
+        if self._match(scanner.TokenType.FALSE):
+            return grammar.Literal(False)
+        if self._match(scanner.TokenType.TRUE):
+            return grammar.Literal(True)
+        if self._match(scanner.TokenType.NIL):
+            return grammar.Literal(None)
+
+        # Checking for string or number literals
+        if self._match(scanner.TokenType.NUMBER, scanner.TokenType.STRING):
+            return grammar.Literal(self._previous().literal)
+
+        # Checking for parenthesized expressions
+        if self._match(scanner.TokenType.LEFT_PAREN):
+            expr = self._expression()
+            # Consume tokens until finding right parenthesis
+            self._consume(scanner.TokenType.RIGHT_PAREN, "Expected ')' after expression")
+            return grammar.Grouping(expr)
+
+    def _consume(self, token_type: scanner.TokenType):
+        """
+        Consumes next token if it is the provided type
+        Raises an error otherwise
+        """
+        if self._check(token_type):
+            return self._advance()
+
+        raise self._error(self._peek(), msg)
+
+    def _error(self, token: scanner.Token, msg: str) -> ParseError:
+        pass
